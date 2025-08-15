260101e79bee8b49e61a2c371635ccd375177f10
Fin-Cottler <fin.cottler@gmail.com>
2025-08-15 18:17:45 -0400

    Update patient.py

--- patient.py
+++ patient.py
@@ -5,6 +5,8 @@ class Patient:
 
     all_patients = []
 
+    education_lvl = {}
+
     def __init__(self, DonorID, ABeta40: float , ABeta42: float, tTau: float, pTau: float):
 
         self.DonorID = DonorID
@@ -28,6 +30,9 @@ class Patient:
 
     def get_id(self):
         return self.DonorID
+    
+    def get_thal(self):
+        return self.thal_score
 
     @classmethod
     def combine_data(cls, filename: str):
@@ -37,14 +42,29 @@ class Patient:
                 #for line in csv create object
                 for row in range(len(rows_of_patients)):
                     if Patient.all_patients[row].DonorID == rows_of_patients[row]["Donor ID"]:
-                        Patient.all_patients[row].sex = rows_of_patients[row]["Sex"]
-                        Patient.all_patients[row].death_age = rows_of_patients[row]["Age at Death"]
-                        Patient.all_patients[row].ed_lvl = rows_of_patients[row]["Highest level of education"]
-                        Patient.all_patients[row].cog_stat = rows_of_patients[row]["Cognitive Status"]
-                        Patient.all_patients[row].age_symp_on = rows_of_patients[row]["Age of onset cognitive symptoms"]
-                        Patient.all_patients[row].age_diag = rows_of_patients[row]["Age of Dementia diagnosis"]
-                        Patient.all_patients[row].head_inj = rows_of_patients[row]["Known head injury"]
-                        Patient.all_patients[row].thal_score = rows_of_patients[row]["Thal"]
+                        if rows_of_patients[row]["Sex"] != "":
+                            Patient.all_patients[row].sex = rows_of_patients[row]["Sex"]
+
+                        if rows_of_patients[row]["Age at Death"] != "":
+                            Patient.all_patients[row].death_age = int(rows_of_patients[row]["Age at Death"])
+
+                        if rows_of_patients[row]["Highest level of education"] != "":
+                            Patient.all_patients[row].ed_lvl = rows_of_patients[row]["Highest level of education"]
+
+                        if rows_of_patients[row]["Cognitive Status"] != "":
+                            Patient.all_patients[row].cog_stat = rows_of_patients[row]["Cognitive Status"]
+
+                        if rows_of_patients[row]["Age of Dementia diagnosis"] != "":
+                            Patient.all_patients[row].age_diag = int(rows_of_patients[row]["Age of Dementia diagnosis"])
+
+                        if rows_of_patients[row]["Known head injury"] != "":
+                            Patient.all_patients[row].head_inj = rows_of_patients[row]["Known head injury"]
+
+                        if rows_of_patients[row]["Thal"] != "":
+                            Patient.all_patients[row].thal_score = int(rows_of_patients[row]["Thal"].split()[1])
+                        
+                        
+            
                     else:
                         warnings.warn("IDs do not match.")
    
@@ -58,11 +78,66 @@ class Patient:
             for row in rows_of_patients:
                 Patient(
                     DonorID = row['Donor ID'],
-                    ABeta40 = row['ABeta40 pg/ug'],
-                    ABeta42 = row['ABeta42 pg/ug'],
-                    tTau = row['tTAU pg/ug'],
-                    pTau = row['pTAU pg/ug']
+                    ABeta40 = float(row['ABeta40 pg/ug']),
+                    ABeta42 = float(row['ABeta42 pg/ug']),
+                    tTau = float(row['tTAU pg/ug']),
+                    pTau = float(row['pTAU pg/ug'])
                 )
             Patient.all_patients.sort(key = Patient.get_id)
             Patient.combine_data(other_file)
+  
+    @classmethod
+    def sort_ed(cls):
+        for patient in Patient.all_patients:
+            Patient.education_lvl.update({patient.ed_lvl: []})
+        for patient in Patient.all_patients:
+            Patient.education_lvl[patient.ed_lvl].append(patient)
+
+    @classmethod
+    def subsort_thal(cls):
+        for key in Patient.education_lvl:
+            values = Patient.education_lvl.get(key)
+            values.sort(key = Patient.get_thal)
+            Patient.education_lvl.update({key: values})
+
+    @classmethod
+    def filter(cls, list, ABeta40:float ="any", ABeta42:float ="any", tTau:float= "any", pTau:float ="any", sex:str ="any", death_age:int ="any", ed_lvl:str ="any", cog_stat:str ="any", age_symp_on:int ="any", age_diag:int ="any", head_inj:str ="any", thal_score:int ="any"):
+        all_patients = list
+        remove_list = []
+        attr_list = (
+                    ABeta40,
+                    ABeta42,
+                    tTau,
+                    pTau,
+                    sex,
+                    death_age,
+                    ed_lvl,
+                    cog_stat,
+                    age_symp_on,
+                    age_diag,
+                    head_inj,
+                    thal_score
+                    )
+        attr_name = (
+                    "ABeta40",
+                    "ABeta42",
+                    "tTau",
+                    "pTau",
+                    "sex",
+                    "death_age",
+                    "ed_lvl",
+                    "cog_stat",
+                    "age_symp_on",
+                    "age_diag",
+                    "head_inj",
+                    "thal_score"
+                    )
+        for attr in range(len(attr_list)):
+            if attr_list[attr] != "any":
+                for patient in all_patients:
+                    if getattr(patient,attr_name[attr]) != attr_list[attr]:
+                        remove_list.append(patient)
+                all_patients = [patient for patient in all_patients if patient not in remove_list]
+                remove_list.clear()
+        return all_patients
   
